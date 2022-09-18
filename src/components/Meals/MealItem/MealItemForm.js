7104a8261ae7fe32ae8db3fa4b8eb4b82d60e262
Ekin Koseoglu <ekindenizkoseoglu@gmail.com>
2022-09-18 16:43:03 +0300

    Forward ref to Input Component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+
 const MealItemForm = (props) => {
+  const amountRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountRef.current.value;
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        label="Amount"
+        label="Amount" // label is a prop of Input component (see src\components\UI\Input.js)
+        ref={amountRef} // <-- here is the ref prop being passed to the Input component (which is a forwardRef component)
         input={{
+          // <-- here is the input prop being passed to the Input component
           type: "number",
           id: "amount_" + props.id,
           min: "1",
